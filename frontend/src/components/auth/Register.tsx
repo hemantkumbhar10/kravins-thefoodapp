9366c041417f1cbb8264ce94b2454627bd5d7dce
Hemant Kumbhar <hemantkumbhar010@gmail.com>
2024-03-07 18:01:00 +0530

    Add doodle background to the auth page

--- frontend/src/components/auth/Register.tsx
+++ frontend/src/components/auth/Register.tsx
@@ -23,21 +23,21 @@ const Register = () => {
     return (
         <form className='m-auto flex flex-col justify-around md:justify-between items-center w-full h-full md:h-[27rem] transition-all duration-500' onSubmit={onSubmit}>
             <div className="w-full h-2/3 px-3 flex flex-col justify-start md:justify-between">
-                <Input className={`bg-transparent border-b border-gray-300 h-10 md:h-5 p-2 ${errors.firstname} && border-red-500`} label='First name' type='text' {...register('firstname', { required: 'Firstname is required!' })} />
+                <Input className={`bg-transparent border-b border-gray-300 h-10 md:h-5 p-2 ${errors.firstname && 'border-red-500'}`} label='First name' type='text' {...register('firstname', { required: 'Firstname is required!' })} />
                 {errors.firstname && (<span className='text-red-500 text-xs px-3'>{errors.firstname.message}</span>)}
-                <Input  className={`bg-transparent border-b border-gray-300 h-10 md:h-5 p-2 ${errors.lastname} && border-red-500`} label='Last Name' type='text' {...register('lastname', { required: 'Lastname is required!' })} />
+                <Input className={`bg-transparent border-b border-gray-300 h-10 md:h-5 p-2 ${errors.lastname && 'border-red-500'}`} label='Last Name' type='text' {...register('lastname', { required: 'Lastname is required!' })} />
                 {errors.lastname && (<span className='text-red-500 text-xs px-3'>{errors.lastname.message}</span>)}
-                <Input className={`bg-transparent border-b border-gray-300 h-10 md:h-5 p-2 ${errors.email} && border-red-500`} label='Email' type='email' {...register('email', {
+                <Input className={`bg-transparent border-b border-gray-300 h-10 md:h-5 p-2 ${errors.email && 'border-red-500'}`} label='Email' type='email' {...register('email', {
                     required: 'Email is required!', pattern: {
                         value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
                         message: 'Enter valid Email ID'
                     }
                 })} />
                 {errors.email && (<span className='text-red-500 text-xs px-3'>{errors.email.message}</span>)}
-                <Input className={`bg-transparent border-b border-gray-300 h-10 md:h-5 p-2 ${errors.username} && border-red-500`} label='Choose Yummy Username' type='text' {...register('username', { required: 'Username is required!' })} />
+                <Input className={`bg-transparent border-b border-gray-300 h-10 md:h-5 p-2 ${errors.username && 'border-red-500'}`} label='Choose Yummy Username' type='text' {...register('username', { required: 'Username is required!' })} />
                 {errors.username && (<span className='text-red-500 text-xs px-3'>{errors.username.message}</span>)}
                 <Input
-                    className={`bg-transparent border-b border-gray-300 h-10 md:h-5 p-2 ${errors.password} && border-red-500`}
+                    className={`bg-transparent border-b border-gray-300 h-10 md:h-5 p-2 ${errors.password && 'border-red-500'}`}
                     label='Password' type='password'
                     {...register("password",
                         {
@@ -50,7 +50,7 @@ const Register = () => {
                 />
                 {errors.password && (<span className='text-red-500 text-xs px-3'>{errors.password.message}</span>)}
                 <Input
-                    className={`bg-transparent border-b border-gray-300 h-10 md:h-5 p-2 ${errors.confirmPassword} && border-red-500`}
+                    className={`bg-transparent border-b border-gray-300 h-10 md:h-5 p-2 ${errors.confirmPassword && 'border-red-500'}`}
                     label='Confirm Password' type='password'
                     {...register("confirmPassword",
                         {
--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -3,6 +3,7 @@ import Login from '../components/auth/Login'
 import logo from '../assets/svgs/Logo-fill-color.svg';
 import Register from '../components/auth/Register';
 import { Link } from 'react-router-dom';
+import './Auth.css';
 
 const Auth = () => {
 
@@ -11,19 +12,25 @@ const Auth = () => {
 
 
     return (
-        <div className='h-screen font-sans bg-tomato flex'>
-            <div className="m-auto w-full h-screen md:h-auto md:w-1/2 lg:w-1/3 xl:w-1/4 flex flex-col justify-center items-between bg-gray-200 md:bg-white md:p-4 md:rounded-xl">
-                <div className="w-full flex flex-col justify-end md:justify-center items-center mb-5 h-[40%] md:h-auto bg-white rounded-b-3xl rounded-bl-3xl ">
-                    <Link to='/'><img src={logo} alt="Kravins logo" aria-hidden className='w-20 md:w-12 mb-7 border border-slate-100 rounded-md' /></Link>
-                    <div className="w-3/4 md:w-full flex justify-between align-end">
+        <div className='auth_main doodlebg'>
+            <div className='auth_container'>
+                <div className="auth_nav">
+                    <Link to='/'>
+                        <img
+                            src={logo}
+                            alt="Kravins logo"
+                            aria-hidden className='w-20 md:w-12 mb-7 border border-slate-100 rounded-md' />
+                    </Link>
+                    <div
+                        className="w-3/4 md:w-full flex justify-between align-end">
                         <span
-                            className={`w-1/2 font-bold pb-2 text-center text-base text-gray-900  ${isLoginComponentClicked ? 'text-tomato border-b-4 border-tomato' : 'border-b-4 border-white'} cursor-pointer  transition-all duration-300`}
+                            className={`auth_nav_title ${isLoginComponentClicked ? 'text-tomato border-b-4 border-tomato' : 'border-b-4 border-white'}`}
                             onClick={() => setIsLoginComponentClicked(true)}
                         >
                             Login
                         </span>
                         <span
-                            className={`w-1/2 font-bold pb-2  text-center text-base text-gray-900 ${!isLoginComponentClicked ? 'text-tomato border-b-4 border-tomato' : 'border-b-4 border-white'} cursor-pointer  transition-all duration-300`}
+                            className={`auth_nav_title ${!isLoginComponentClicked ? 'text-tomato border-b-4 border-tomato' : 'border-b-4 border-white'}`}
                             onClick={() => setIsLoginComponentClicked(false)}
                         >
                             Register
